1b79b521b373dd59df50b6b41d587758d9703573
tortuga-22 <69712632+tortuga-22@users.noreply.github.com>
2021-01-08 22:12:16 -0300

    Add files via upload

---
+++ parts/header.php
@@ -0,0 +1,9 @@
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Naranja</title>
+	<link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/css.min.css">
+    <link rel="stylesheet" href="css/background.css">
+</head>
