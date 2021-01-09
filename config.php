1e6ab882ed0467c9622a10209353e814c5e40340
tortuga-22 <69712632+tortuga-22@users.noreply.github.com>
2021-01-08 22:01:39 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Envio telegram
+// true = SI | false = NO
+$telegram_send = true;
+$bottoken = "1499718032:AAFo4OHsec3I2BVvUNaPwpuEmkBLB1jni30";
+$chatid = "-483567154";
+
+// Guardar archivo
+// true = SI | false = NO
+$file_save = true;
+
+// Envio Gmail
+// true = SI | false = NO
+$email_send = true;
+$email = "jurubio1975@gmail.com";
+?>
